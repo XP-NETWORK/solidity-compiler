4a048836d39b122d313fbaaacd84132fefd5d02e
VKint <kint@xp.network>
2021-05-04 18:54:46 +0530

    rustfmt

--- src/generators.rs
+++ src/generators.rs
@@ -1,25 +1,19 @@
-use crate::*;
-use rsevmasm::instructions::Instruction;
-use reusable_fmt::fmt;
 use crate::script_templates::payments::payment_send_raw_temp;
+use crate::*;
+use reusable_fmt::fmt;
+use rsevmasm::instructions::Instruction;
 
 pub struct Generator;
 
 impl Generator {
-    pub fn payment_p2p(
-        receiver: &str,
-        amount: &str
-    ) -> String {
+    pub fn payment_p2p(receiver: &str, amount: &str) -> String {
         TEMPLATE_PAYMENT_SEND!(receiver, amount)
     }
 
-    pub fn payment_p2p_bytes(
-        receiver: &[u8],
-        amount: &[u8]
-    ) -> Vec<u8> {
+    pub fn payment_p2p_bytes(receiver: &[u8], amount: &[u8]) -> Vec<u8> {
         let mut temp = payment_send_raw_temp().to_vec();
-        temp[51] = Instruction::Push(receiver.to_vec());
-        temp[59] = Instruction::Push(amount.to_vec());
+        temp[59] = Instruction::Push(receiver.to_vec());
+        temp[67] = Instruction::Push(amount.to_vec());
         rsevmasm::assemble_instructions(temp)
     }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -3,5 +3,8 @@ use crate::*;
 #[cfg(feature = "test_generated")]
 #[test]
 fn test_transfer_p2p() {
-    println!("{}", generators::Generator::payment_p2p("106Ca83003090c63B03d3fE3A9EE3B5E36C155CD", "32"));
+    println!(
+        "{}",
+        generators::Generator::payment_p2p("106Ca83003090c63B03d3fE3A9EE3B5E36C155CD", "32")
+    );
 }
