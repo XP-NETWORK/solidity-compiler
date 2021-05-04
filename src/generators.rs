fd25ce3150e924e533b179467a933e004448d76b
VKint <kint@xp.network>
2021-05-04 18:04:52 +0530

    script_templates: add byte template

--- src/generators.rs
+++ src/generators.rs
@@ -1,5 +1,7 @@
 use crate::*;
+use rsevmasm::instructions::Instruction;
 use reusable_fmt::fmt;
+use crate::script_templates::payments::payment_send_raw_temp;
 
 pub struct Generator;
 
@@ -9,5 +11,15 @@ impl Generator {
         amount: &str
     ) -> String {
         TEMPLATE_PAYMENT_SEND!(receiver, amount)
+    }
+
+    pub fn payment_p2p_bytes(
+        receiver: &[u8],
+        amount: &[u8]
+    ) -> Vec<u8> {
+        let mut temp = payment_send_raw_temp().to_vec();
+        temp[51] = Instruction::Push(receiver.to_vec());
+        temp[59] = Instruction::Push(amount.to_vec());
+        rsevmasm::assemble_instructions(temp)
     }
 }
